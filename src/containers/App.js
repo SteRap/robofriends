3decd542b965bc358b094f3116c19195f5bcec8d
SteRap <92691510+SteRap@users.noreply.github.com>
2022-01-02 11:44:15 +0100

    fix title size

--- src/containers/App.js
+++ src/containers/App.js
@@ -31,7 +31,7 @@ class App extends Component {
       <h1>Loading</h1>
     ) : (
       <div className="tc">
-        <h1 className="f1">RoboFriends</h1>
+        <h1 className="title">RoboFriends</h1>
         <SearchBox searchChange={this.onSearchChange} />
         <Scroll>
           <CardList robots={filteredRobots} />
